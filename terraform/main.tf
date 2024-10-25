5953edde33da85eb80220972bbd553da0be95def
ludsonfelipe <felipe_ludson@hotmail.com>
2024-10-25 19:23:01 -0300

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ provider "google" {
 
 module "project-services" {
   source                      = "terraform-google-modules/project-factory/google//modules/project_services"
-  version                     = "14.4.0"
+  version                     = "17.0.0"
   disable_services_on_destroy = false
 
   project_id  = var.project
@@ -22,38 +22,26 @@ module "project-services" {
   ]
 }
 
-
-resource "google_service_account" "my_service_account" {
-  account_id   = "pipeline"
-  display_name = "Pipeline Service Account"
-}
-
-resource "google_project_iam_binding" "pubsub_publisher" {
-  project = var.project
-  role    = "roles/pubsub.publisher"
-
-  members = [
-    "serviceAccount:${google_service_account.my_service_account.email}"
-  ]
-}
-
+# Get the service account for pubsub
 resource "google_project_service_identity" "pubsub_id" {
   provider = google-beta
   project = var.project
   service = "pubsub.googleapis.com"
 }
 
+# Grant the service account the necessary permissions to access the storage bucket
 resource "google_storage_bucket_iam_member" "user_pubsub_storage" {
   bucket = module.buckets.bucket_name_pubsub
   role   = "roles/storage.admin"
   member = "serviceAccount:${google_project_service_identity.pubsub_id.email}"
 }
 
-
+# Generate a random bucket id
 resource "random_id" "bucket_id" {
   byte_length = 4
 }
 
+# Create the buckets
 module "buckets" {
   source = "./buckets"
 
@@ -62,6 +50,7 @@ module "buckets" {
   location = "US"
 }
 
+# Create the database
 module "database" {
 
   source = "./database"
@@ -75,14 +64,31 @@ module "database" {
   depends_on = [ module.project-services ]
 }
 
-module "instances" {
+# Create the pubsub topic and subscription
+module "pubsub" {
+  source = "./pubsub"
+  ecom_topic_name = "ecom_topic"
+  ecom_subscription_name = "ecom_subscription"
+  bucket_name_pubsub = module.buckets.bucket_name_pubsub
+  depends_on = [ 
+    module.buckets.bucket_name_pubsub,
+    google_storage_bucket_iam_member.user_pubsub_storage,
+    ]
+}
 
-  source = "./instance"
-  address = module.database.database_ip
-  repository = var.repo
+resource "google_service_account" "my_service_account" {
+  account_id   = "pipeline"
+  display_name = "Pipeline Service Account"
+}
+
+# Grant the service account the necessary permissions to publish to the pubsub topic
+resource "google_project_iam_binding" "pubsub_publisher" {
   project = var.project
-  account = google_service_account.my_service_account.email
-  depends_on = [ google_project_iam_binding.pubsub_publisher ]
+  role    = "roles/pubsub.publisher"
+
+  members = [
+    "serviceAccount:${google_service_account.my_service_account.email}"
+  ]
 }
 
 module "datastreams" {
@@ -102,13 +108,13 @@ module "datastreams" {
   depends_on = [ module.project-services ]
 }
 
-module "pubsub" {
-  source = "./pubsub"
-  ecom_topic_name = "ecom_topic"
-  ecom_subscription_name = "ecom_subscription"
-  bucket_name_pubsub = module.buckets.bucket_name_pubsub
-  depends_on = [ 
-    module.buckets.bucket_name_pubsub,
-    google_storage_bucket_iam_member.user_pubsub_storage,
-    ]
+# Create the instance that will generate the data
+module "instances" {
+
+  source = "./instance"
+  address = module.database.database_ip
+  repository = var.repo
+  project = var.project
+  account = google_service_account.my_service_account.email
+  depends_on = [ google_project_iam_binding.pubsub_publisher ]
 }
